8b626d83b9ab7a2b1bb54c36e9ad5db8dea24d2b
Rishav Anand <rishavdipsite@gmail.com>
2020-05-04 22:50:40 +0530

    connect product create form to API

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,47 +1,115 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState, ChangeEvent } from 'react';
 import { Form, Row, Col } from 'react-bootstrap';
 import styles from './Form.module.scss';
 import { PrimaryButton } from '../button/Button';
+import { APIRequester, REQUEST_METHODS } from '../../helpers/apiRequester';
+import store from 'store';
+
+type FormControlElement = HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement;
 
 export interface FormProps {
     sections: {
-        id: string;
-        name: string;
-        description: string;
-    }[];
+        [id: string]: {
+            name: string;
+            description: string;
+        };
+    };
     fields: {
-        name: string;
-        type: string;
-        required: boolean;
-        sectionId: string;
-    }[];
+        [id: string]: {
+            name: string;
+            type: string;
+            required: boolean;
+            sectionId: string;
+            value: any;
+        };
+    };
+    submitUrl: string;
+    onSuccess: (state: number) => void;
+    onError: (message: string) => void;
 }
 
 export const PrimaryForm: FunctionComponent<FormProps> = props => {
-    const fields = props.fields;
+    const [fields, setFields] = useState(props.fields);
     const sections = props.sections;
+    const onSuccess = props.onSuccess;
+    const onError = props.onError;
+    const [processing, setProcessing] = useState(false);
+    const submitUrl = props.submitUrl;
+
+    const onChange = (e: React.ChangeEvent<FormControlElement>) => {
+        e.preventDefault();
+        setFields({
+            ...fields,
+            [e.target.name]: { ...fields[e.target.name], value: e.target.value },
+        });
+    };
+
+    const validateForm = async () => {
+        Object.keys(fields).map(fieldId => {
+            if (!fields[fieldId].value && fields[fieldId].required)
+                throw new Error(`Please enter a valid ${fields[fieldId].name}`);
+        });
+    };
+
+    const onClick = async () => {
+        try {
+            setProcessing(true);
+            onSuccess(0);
+            onError('');
+
+            await validateForm();
+            const body: { [key: string]: any } = {};
+            Object.keys(fields).map(fieldId =>
+                fields[fieldId].value ? (body[fieldId] = fields[fieldId].value) : undefined,
+            );
+            const token = store.get('token');
+            const req = new APIRequester({ url: submitUrl })
+                .setMethod(REQUEST_METHODS.POST)
+                .setBody(body)
+                .setHeaders({
+                    Authorization: `Bearer ${token}`,
+                })
+                .request();
+            const res = await req;
+            const resJson = await res.json();
+
+            console.log(resJson);
+
+            onSuccess(1);
+            setProcessing(false);
+        } catch (err) {
+            setProcessing(false);
+            console.log(err);
+            if (err.title) return onError(err.title);
+            return onError(err.message);
+        }
+    };
+
     return (
         <>
-            {sections.map(section => {
+            {Object.keys(sections).map(sectionId => {
                 return (
                     <>
                         <Row>
                             <Col md={6} xs={12}>
-                                <div className={styles.sectionName}>{section.name}</div>
-                                <div className={styles.sectionDescription}>{section.description}</div>
+                                <div className={styles.sectionName}>{sections[sectionId].name}</div>
+                                <div className={styles.sectionDescription}>{sections[sectionId].description}</div>
                             </Col>
                             <Col md={6} xs={12}>
-                                {fields.map(field => {
-                                    if (field.sectionId === section.id) {
-                                        return (
-                                            <Form>
-                                                <Form.Group>
-                                                    <Form.Label>{field.name}</Form.Label>
-                                                    <Form.Control type={field.type} />
-                                                </Form.Group>
-                                            </Form>
-                                        );
-                                    }
+                                {Object.keys(fields).map(fieldId => {
+                                    return fields[fieldId].sectionId === sectionId ? (
+                                        <Form>
+                                            <Form.Group>
+                                                <Form.Label>{fields[fieldId].name}</Form.Label>
+                                                <Form.Control
+                                                    name={fieldId}
+                                                    type={fields[fieldId].type}
+                                                    value={fields[fieldId].value}
+                                                    onChange={e => onChange(e)}
+                                                />
+                                            </Form.Group>
+                                        </Form>
+                                    ) : undefined;
                                 })}
                             </Col>
                         </Row>
@@ -52,7 +120,9 @@ export const PrimaryForm: FunctionComponent<FormProps> = props => {
             <Row>
                 <Col md={6} xs={0}></Col>
                 <Col md={6} xs={12}>
-                    <PrimaryButton fullWidth>Create</PrimaryButton>
+                    <PrimaryButton processing={processing} onClick={onClick} fullWidth>
+                        Create
+                    </PrimaryButton>
                 </Col>
             </Row>
         </>
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,28 +1,91 @@
-import React, { FunctionComponent } from 'react';
-import { Link } from 'react-router-dom';
-import { Form, InputGroup } from 'react-bootstrap';
+import React, { FunctionComponent, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { Form, InputGroup, Alert } from 'react-bootstrap';
 import { FullLayout } from '../layout/FullLayout';
 import styles from './Login.module.scss';
 import { PrimaryButton, SecondaryInverseButton } from '../button/Button';
+import { APIRequester, REQUEST_METHODS } from '../../helpers/apiRequester';
+import store from 'store';
+
+type FormControlElement = HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement;
 
 export const Login: FunctionComponent = props => {
+    const rawFields: { [key: string]: string } = {
+        email: '',
+        password: '',
+    };
+    const [token, setToken] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [fields, setFields] = useState(rawFields);
+
+    const onChange = (e: React.ChangeEvent<FormControlElement>) => {
+        e.preventDefault();
+        setFields({
+            ...fields,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const validateForm = async () => {
+        Object.keys(fields).map(fieldId => {
+            if (!fields[fieldId]) throw new Error(`Please enter a valid ${fieldId}`);
+        });
+    };
+
+    const onClick = async () => {
+        try {
+            setErrorMessage('');
+            await validateForm();
+            const submitUrl = `${process.env.REACT_APP_API_GATEWAY}/api/v1/login/basic`;
+            const req = new APIRequester({ url: submitUrl }).setMethod(REQUEST_METHODS.POST).setBody(fields).request();
+            const res = await req;
+            const resJson: { token: string } = await res.json();
+            const token = resJson.token;
+            store.set('token', resJson.token);
+            setToken(token);
+        } catch (err) {
+            console.log(err);
+            if (err.title) return setErrorMessage(err.title);
+            return setErrorMessage(err.message);
+        }
+    };
+
+    const errorAlert = errorMessage ? <Alert variant="danger">{errorMessage}</Alert> : undefined;
+
+    if (token) return <Redirect to="/products" />;
+
     return (
         <FullLayout>
             <div className={styles.mainBox}>
+                {errorAlert}
                 <Form>
                     <Form.Group>
                         <InputGroup>
                             <InputGroup.Prepend>
                                 <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
                             </InputGroup.Prepend>
-                            <Form.Control type="email" placeholder="Enter your email address" />
+                            <Form.Control
+                                name="email"
+                                type="email"
+                                placeholder="Enter your email address"
+                                value={fields.email}
+                                onChange={onChange}
+                            />
                         </InputGroup>
                     </Form.Group>
                     <Form.Group>
-                        <Form.Control type="password" placeholder="Enter your password" />
+                        <Form.Control
+                            name="password"
+                            type="password"
+                            placeholder="Enter your password"
+                            value={fields.password}
+                            onChange={onChange}
+                        />
                     </Form.Group>
                     <Form.Group>
-                        <PrimaryButton fullWidth={true}>Login</PrimaryButton>
+                        <PrimaryButton onClick={onClick} fullWidth={true}>
+                            Login
+                        </PrimaryButton>
                     </Form.Group>
                     <Form.Group className={styles.buttonBox}>
                         <Link to="/register">
